13ad60c26fc147f042e66ef4b8f65f888a61207a
Daniel Schaefer <git@danielschaefer.me>
2026-03-21 12:01:39 +0800

    freebsd: use lldb for debugging
    Signed-off-by: Daniel Schaefer <git@danielschaefer.me>

--- contrib/launch-venv.sh
+++ contrib/launch-venv.sh
@@ -10,11 +10,13 @@ export FWUPD_LOCALSTATEDIR=${DIST}
 export FWUPD_SYSCONFDIR=${DIST}/etc
 export LD_LIBRARY_PATH=${DIST}/lib/${gcc}:${DIST}/lib64:${DIST}/lib
 if [ -n "${DEBUG}" ]; then
-    if ! which gdbserver 1>/dev/null 2>&1; then
+    DEBUG="gdbserver localhost:9091"
+    if which lldb 1>/dev/null 2>&1; then
+        DEBUG="lldb"
+    elif ! which gdbserver 1>/dev/null 2>&1; then
         echo "install gdbserver to enable debugging"
         exit 1
     fi
-    DEBUG="gdbserver localhost:9091"
 fi
 if [ -f ${DIST}/libexec/fwupd/${BIN} ]; then
     EXE=${DIST}/libexec/fwupd/${BIN}
